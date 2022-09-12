aca6ba39b86b08c961b33d7e89f4666eb50b7501
mdayican <mustafa.dayican@hmcts.net>
2022-09-12 21:56:20 +0100

    fix module name

--- application-insights.tf
+++ application-insights.tf
@@ -8,7 +8,7 @@ resource "azurerm_application_insights" "appinsights" {
 resource "azurerm_key_vault_secret" "app_insights_connection_string" {
   name         = "app-insights-connection-string"
   value        = azurerm_application_insights.appinsights.connection_string
-  key_vault_id = module.vault.key_vault_id
+  key_vault_id = module.key_vault.key_vault_id
 }
   
 output "appInsightsInstrumentationKey" {
