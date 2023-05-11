0aacae0f9577cdcda546a150fab6fc0ca05961c3
yogesh-hullatti <yogesh.hullatti@hmcts.net>
2023-05-11 11:18:05 +0100

    Updated tags to reflect EM details

--- application-insights.tf
+++ application-insights.tf
@@ -23,6 +23,7 @@ resource "azurerm_application_insights" "em_appinsights" {
   location            = var.location
   resource_group_name = azurerm_resource_group.rg.name
   application_type    = var.application_type
+  tags                = local.tags
 }
 
 resource "azurerm_key_vault_secret" "em_app_insights_connection_string" {
@@ -31,6 +32,13 @@ resource "azurerm_key_vault_secret" "em_app_insights_connection_string" {
   key_vault_id = module.key_vault.key_vault_id
 }
 
+resource "azurerm_key_vault_secret" "em_app_insights_key" {
+  name         = "EmAppInsightsInstrumentationKey"
+  value        = azurerm_application_insights.em_appinsights.instrumentation_key
+  key_vault_id = module.key_vault.key_vault_id
+}
+
 output "em_appInsightsInstrumentationKey" {
+  sensitive = true
   value = azurerm_application_insights.em_appinsights.instrumentation_key
 }
--- main.tf
+++ main.tf
@@ -2,8 +2,22 @@ provider "azurerm" {
   features {}
 }
 
+locals {
+  tags = merge(
+    var.common_tags,
+    tomap({
+      "Team Contact"        = var.team_contact
+      "Destroy Me"          = var.destroy_me
+      "application"         = "evidence-management"
+      "managedBy"           = "Evidence Management"
+      "businessArea"        = "CFT"
+      "contactSlackChannel" = var.team_contact
+    })
+  )
+}
+
 resource "azurerm_resource_group" "rg" {
   name     = "${var.product}-${var.env}"
   location = var.location
-  tags = var.common_tags
+  tags = local.tags
 }
